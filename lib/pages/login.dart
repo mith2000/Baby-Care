81536caa796afb833e6a6dbfbd31c68ba062f9c1
Thang <58561617+Thangnguyen11@users.noreply.github.com>
2021-04-15 14:56:31 +0700

    Init Dependencies & Add LoginPage
    - Init Dependencies in pubspec.yaml
    - Add Fonts
    - Add LoginPage
    - Remove old main.dart

---
+++ lib/pages/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
