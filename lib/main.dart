11e09623414290be311c1a00a7d30ee8768923f4
Thang <58561617+Thangnguyen11@users.noreply.github.com>
2021-04-15 16:38:42 +0700

    Add HexColor & LoginPage background
    - Add HexColor
    - LoginPage background

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter/rendering.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import 'pages/login.dart';
 
@@ -28,17 +29,18 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        //brightness dart will overwrite the primarySwatch and background
-        brightness: Brightness.light,
-        //main color
-        primaryColor: Colors.pink[100],
-        //if dont define what color using, automatically use this
-        primarySwatch: Colors.red,
-        //backbuffer color :v just scroll out a list
-        accentColor: Colors.grey[400],
-        buttonColor: Colors.pinkAccent[200],
-        indicatorColor: Colors.pink[100],
-      ),
+          //brightness overwrite primarySwatch and background
+          brightness: Brightness.light,
+          primaryColor: HexColor('#05dbf2'),
+          //if dont define what color using, automatically use this
+
+          //backbuffer color :v just scroll out a list
+          // accentColor: HexColor('#b3ecf2'),
+          buttonColor: HexColor('#f2b3e1'),
+          // indicatorColor: HexColor('#f2b3e1'),
+
+          backgroundColor: HexColor('#b3ecf2'),
+          cardColor: HexColor('#f2bdcb')),
 
       //routes as shortcuts for Navigator
       routes: {
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -12,7 +13,20 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Theme.of(context).cardColor,
+                Theme.of(context).backgroundColor,
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
