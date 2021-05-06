ee979fb43ee105b6591f1b1f624c0798673d5444
Thang <58561617+Thangnguyen11@users.noreply.github.com>
2021-05-06 16:38:30 +0700

    Add new widget in GlassContainer
    - Add new widget in GlassContainer

--- lib/pages/catalog.dart
+++ lib/pages/catalog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:glass_kit/glass_kit.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class CatalogPage extends StatelessWidget {
   @override
@@ -41,12 +42,363 @@ class CatalogPage extends StatelessWidget {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 30),
+                      padding: const EdgeInsets.only(left: 30, bottom: 10),
                       child: Text(
                         'Hope your angels are well',
                         style: Theme.of(context).textTheme.button,
                         overflow: TextOverflow.ellipsis,
                       ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(),
+                        GestureDetector(
+                          child: Container(
+                            width: 351,
+                            height: 138,
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    HexColor('#7ed1f2'),
+                                    HexColor('#bd88f2'),
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter),
+                              borderRadius: BorderRadius.circular(32.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: HexColor('#bd88f2').withOpacity(0.5),
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, 8),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 20),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 8, top: 5),
+                                      child: Text(
+                                        'Baby Health',
+                                        style:
+                                            Theme.of(context).textTheme.button,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 124,
+                                      child: Text(
+                                        'Checking the health stutus of your baby',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Expanded(child: Container()),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: FadeInImage(
+                                    placeholder: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                    height: 128,
+                                    width: 128,
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                  ),
+                                ),
+                                SizedBox(width: 26),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            print('tapped');
+                          },
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            width: 351,
+                            height: 138,
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    HexColor('#7ed1f2'),
+                                    HexColor('#bd88f2'),
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter),
+                              borderRadius: BorderRadius.circular(32.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: HexColor('#bd88f2').withOpacity(0.5),
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, 8),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 20),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 8, top: 5),
+                                      child: Text(
+                                        'Baby Health',
+                                        style:
+                                            Theme.of(context).textTheme.button,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 124,
+                                      child: Text(
+                                        'Checking the health stutus of your baby',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Expanded(child: Container()),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: FadeInImage(
+                                    placeholder: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                    height: 128,
+                                    width: 128,
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                  ),
+                                ),
+                                SizedBox(width: 26),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            print('tapped');
+                          },
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            width: 351,
+                            height: 138,
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    HexColor('#7ed1f2'),
+                                    HexColor('#bd88f2'),
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter),
+                              borderRadius: BorderRadius.circular(32.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: HexColor('#bd88f2').withOpacity(0.5),
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, 8),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 20),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 8, top: 5),
+                                      child: Text(
+                                        'Baby Health',
+                                        style:
+                                            Theme.of(context).textTheme.button,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 124,
+                                      child: Text(
+                                        'Checking the health stutus of your baby',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Expanded(child: Container()),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: FadeInImage(
+                                    placeholder: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                    height: 128,
+                                    width: 128,
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                  ),
+                                ),
+                                SizedBox(width: 26),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            print('tapped');
+                          },
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            width: 351,
+                            height: 138,
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    HexColor('#7ed1f2'),
+                                    HexColor('#bd88f2'),
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter),
+                              borderRadius: BorderRadius.circular(32.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: HexColor('#bd88f2').withOpacity(0.5),
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, 8),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 20),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 8, top: 5),
+                                      child: Text(
+                                        'Baby Health',
+                                        style:
+                                            Theme.of(context).textTheme.button,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 124,
+                                      child: Text(
+                                        'Checking the health stutus of your baby',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Expanded(child: Container()),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: FadeInImage(
+                                    placeholder: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                    height: 128,
+                                    width: 128,
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                  ),
+                                ),
+                                SizedBox(width: 26),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            print('tapped');
+                          },
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            width: 351,
+                            height: 138,
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    HexColor('#7ed1f2'),
+                                    HexColor('#bd88f2'),
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter),
+                              borderRadius: BorderRadius.circular(32.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: HexColor('#bd88f2').withOpacity(0.5),
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, 8),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 20),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 8, top: 5),
+                                      child: Text(
+                                        'Baby Health',
+                                        style:
+                                            Theme.of(context).textTheme.button,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 124,
+                                      child: Text(
+                                        'Checking the health stutus of your baby',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Expanded(child: Container()),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: FadeInImage(
+                                    placeholder: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                    height: 128,
+                                    width: 128,
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        'assets/image/baby_default.jpg'),
+                                  ),
+                                ),
+                                SizedBox(width: 26),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            print('tapped');
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 ),
