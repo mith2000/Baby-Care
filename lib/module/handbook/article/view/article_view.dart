a39c9c8b0f9a195b4d5b00939b2c686e1be3af8f
Thang Nguyen <thangnm2k@gmail.com>
2022-03-27 11:25:58 +0700

    Back btn in ArticleView

--- lib/module/handbook/article/view/article_view.dart
+++ lib/module/handbook/article/view/article_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_babycare/constants/app_constants.dart';
+import 'package:flutter_babycare/utils/UI_components/line_button.dart';
 import 'package:flutter_babycare/utils/app_colors.dart';
 
 class ArticleView extends StatefulWidget {
@@ -13,6 +15,34 @@ class ArticleView extends StatefulWidget {
 class _ArticleViewState extends State<ArticleView> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: AppColors.background);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        color: AppColors.background,
+        child: Stack(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                _buildBackButton(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: AppConstants.paddingAppW,
+        vertical: AppConstants.paddingAppH,
+      ),
+      child: LineButton('Back', () {
+        Navigator.pop(context);
+      }),
+    );
   }
 }
