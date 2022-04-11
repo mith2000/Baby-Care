af6f8362e489ca3ad8381d968b9d35a0ab9a6d16
Thang Nguyen <thangnm2k@gmail.com>
2022-04-11 14:32:11 +0700

    Done Chat center

--- lib/module/chatcenter/center/view/chat_center.dart
+++ lib/module/chatcenter/center/view/chat_center.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_babycare/module/chatcenter/p2p/view/chat_p2p.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../constants/app_constants.dart';
 import '../../../../utils/app_colors.dart';
 
 class ChatCenterView extends StatefulWidget {
@@ -14,6 +17,138 @@ class ChatCenterView extends StatefulWidget {
 class _ChatCenterViewState extends State<ChatCenterView> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: AppColors.danger);
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: AppConstants.paddingAppW,
+        vertical: AppConstants.paddingAppH,
+      ),
+      color: AppColors.background,
+      height: double.infinity,
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: ScrollPhysics(),
+        itemCount: 1,
+        itemBuilder: (context, index) {
+          return _buildThemeButton(
+            title: 'Caring center',
+            description: 'Get advices by chatting with us',
+            action: () {
+              Navigator.pushNamed(
+                context,
+                ChatP2PView.routeName,
+                arguments: ChatP2PViewArguments('Caring center'),
+              );
+            },
+            image: AssetImage('assets/image/logo_s150.png'),
+            isRedFrame: index % 2 == 1 ? true : false,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildThemeButton({
+    String title,
+    String description,
+    Function action,
+    ImageProvider image,
+    bool isRedFrame = false,
+  }) {
+    return Container(
+      height: 132.h,
+      margin: EdgeInsets.symmetric(vertical: AppConstants.paddingNormalH),
+      child: ElevatedButton(
+        onPressed: action,
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 24.sp,
+                          color: AppColors.whiteBackground,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: AppConstants.paddingNormalH),
+                    Container(
+                      child: Text(
+                        description,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22.sp,
+                          color: AppColors.whiteBackground,
+                        ),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: AppConstants.paddingNormalW),
+              Container(
+                height: double.infinity,
+                margin: EdgeInsets.only(
+                    top: AppConstants.paddingNormalH,
+                    bottom: AppConstants.paddingLargeH),
+                child: ClipRRect(
+                  borderRadius:
+                      BorderRadius.circular(AppConstants.cornerRadiusFrame),
+                  child: Image(
+                    width: 100.w,
+                    fit: BoxFit.cover,
+                    image: image,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          padding: EdgeInsets.only(
+            top: AppConstants.paddingNormalH,
+            left: AppConstants.paddingLargeW,
+            right: AppConstants.paddingLargeW,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: isRedFrame == false
+                  ? [
+                      AppColors.primary,
+                      AppColors.secondary,
+                    ]
+                  : [
+                      AppColors.danger,
+                      AppColors.highlighter,
+                    ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.circular(AppConstants.cornerRadiusFrame),
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.shadow,
+                blurRadius: 4,
+                offset: Offset(0, 4), // changes position of shadow
+              ),
+            ],
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size.zero,
+          padding: EdgeInsets.zero,
+          primary: AppColors.primary,
+          onPrimary: AppColors.solidButtonPress,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppConstants.cornerRadiusFrame),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/module/chatcenter/p2p/view/chat_p2p.dart
+++ lib/module/chatcenter/p2p/view/chat_p2p.dart
@@ -3,6 +3,12 @@ import 'package:flutter_babycare/utils/app_colors.dart';
 
 import '../../../../utils/app_colors.dart';
 
+class ChatP2PViewArguments {
+  final String peerName;
+
+  ChatP2PViewArguments(this.peerName);
+}
+
 class ChatP2PView extends StatefulWidget {
   static const routeName = '/chat-p2p';
 
@@ -15,6 +21,9 @@ class ChatP2PView extends StatefulWidget {
 class _ChatP2PViewState extends State<ChatP2PView> {
   @override
   Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context).settings.arguments as ChatP2PViewArguments;
+    print(args.peerName);
     return Container(color: AppColors.background);
   }
 }
